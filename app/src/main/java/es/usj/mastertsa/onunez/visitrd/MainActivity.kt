13ee6675692d4375b55c69673cbf7fd38eaa74c5
Odalis <odalislnunez@hotmail.com>
2021-12-20 06:16:32 -0400

    Adding Place Activity and adding main menu to main activity.

--- app/src/main/java/es/usj/mastertsa/onunez/visitrd/MainActivity.kt
+++ app/src/main/java/es/usj/mastertsa/onunez/visitrd/MainActivity.kt
@@ -2,9 +2,16 @@ package es.usj.mastertsa.onunez.visitrd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +24,9 @@ class MainActivity : AppCompatActivity() {
         val adapter = PlacesAdapter(this, placeList)
 
         lvMain.adapter = adapter
+
+        lvMain.setOnItemClickListener { parent, view, position, id ->
+
+        }
     }
 }
